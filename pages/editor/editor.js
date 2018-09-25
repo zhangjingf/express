38173a15d7fd9eae4e4502f78e0c51c82cab95fd
zhangjingfeng <734546749@qq.com>
2018-09-25 14:05:31 +0800

    开发

--- pages/editor/editor.js
+++ pages/editor/editor.js
@@ -1,10 +1,12 @@
 // pages/editor/editor.js
 Page({
   data: {
-    visible1: true
+    visible1: false
   },
   onLoad: function (options) {
-  
+    wx.setBackgroundColor({
+      backgroundColor: '#e5e5e5'
+    })
   },
   onReady: function () {
   

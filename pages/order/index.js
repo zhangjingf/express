6cb0a39146cc2300b4bcdba5b9e0d60d09e11d3e
zhangjingfeng <734546749@qq.com>
2018-09-26 16:47:21 +0800

    开发

---
+++ pages/order/index.js
@@ -0,0 +1,26 @@
+Page({
+  data: {
+    orderList: [{
+      id: 11,
+      name: '张金发个',
+      phone: '112312321',
+      type: '1',
+      time: '2018-09-26 16:31',
+      address: '小时多吃点说多错多',
+      bookingTime: '今天18:00-18:30',
+      status: '11',
+      expressType: '申通快递',
+      orderNum: '1111111111111',
+      money: '11'
+    }]
+  },
+  onLoad: function (options) {
+    wx.setNavigationBarTitle({
+      title: '订单',
+    })
+  },
+  onReady: function () {
+    var self = getApp()
+    console.log(self.globalData)
+  }
+})
--- pages/send/index.js
+++ pages/send/index.js
@@ -1,7 +1,15 @@
 // pages/send/index.js
 Page({
   data: {
-
+    isChecked: null,
+    color: '#2a82e4',
+    wordNum: '0',
+    message: null,
+    index: 0,
+    thingTypeList: ['美国', '中国', '巴西', '日本'],
+    multiArray: [['无脊柱动物', '脊柱动物'], ['扁性动物', '线形动物', '环节动物', '软体动物', '节肢动物']],
+    multiIndex: [0, 0],
+    switchColor: "#e7e7e7"
   },
   onLoad: function (options) {
     wx.setNavigationBarTitle({
@@ -10,5 +18,26 @@ Page({
   },
   onReady: function () {
 
+  },
+  bindInput: function (e) {
+    this.setData({
+      message: e.detail.value,
+      wordNum: e.detail.value.length
+    })
+  },
+  bindTypeChange: function (e) {
+    this.setData({
+      index: e.detail.value
+    })
+  },
+  bindTimeChange: function (e) {
+    this.setData({
+      multiIndex: e.detail.value
+    })
+  },
+  goMyAddress: function (e) {
+    wx.navigateTo({
+      url: '../myAddress/index',
+    })
   }
 })

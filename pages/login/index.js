8806c655d7a233be84bef6d5931493c94f772555
zhangjingf <734546749@qq.com>
2018-10-09 23:39:27 +0800

    开发

--- pages/login/index.js
+++ pages/login/index.js
@@ -10,43 +10,7 @@ Page({
         if (res.authSetting['scope.userInfo']) {
           wx.getUserInfo({
             success: function (res) {
-              app.globalData.userInfo = res.userInfo
-              var param = {
-                code: app.globalData.code,
-                userInfo: {
-                  userInfo: {
-                    country: res.userInfo.country,
-                    gender: res.userInfo.gender,
-                    province: res.userInfo.province,
-                    city: res.userInfo.city,
-                    avatarUrl: res.userInfo.avatarUrl,
-                    nickName: res.userInfo.nickName,
-                    language: res.userInfo.language
-                  },
-                  signature: res.signature,
-                  errMsg: res.errMsg,
-                  encryptedData: res.encryptedData,
-                  iv: res.iv,
-                  rawData: JSON.stringify(JSON.parse(res.rawData))
-                }
-              }
-              if (res.userInfo) {
-                login.wxBindLogin(param, function (res) {
-                  if (res.errno == 0) {
-                    wx.setStorage({
-                      key: "token",
-                      data: res.data.token
-                    })
-                    wx.setStorage({
-                      key: 'userId',
-                      data: res.data.userId
-                    })
-                    wx.redirectTo({
-                      url: '../main/main',
-                    })
-                  }
-                })
-              }
+              this.login(res);
             }
           });
         }
@@ -54,46 +18,10 @@ Page({
     })
   },
   bindGetUserInfo: function (e) {
-    var res = e.detail
+    var res = e.detail;
     if (res) {
-      var param = {
-        code: app.globalData.code,
-        userInfo: {
-          userInfo: {
-            country: res.userInfo.country,
-            gender: res.userInfo.gender,
-            province: res.userInfo.province,
-            city: res.userInfo.city,
-            avatarUrl: res.userInfo.avatarUrl,
-            nickName: res.userInfo.nickName,
-            language: res.userInfo.language
-          },
-          signature: res.signature,
-          errMsg: res.errMsg,
-          encryptedData: res.encryptedData,
-          iv: res.iv,
-          rawData: JSON.stringify(JSON.parse(res.rawData))
-        }
-      }
-      if (res.userInfo) {
-        login.wxBindLogin(param, function (res) {
-          if (res.errno == 0) {
-            wx.setStorage({
-              key: "token",
-              data: res.data.token
-            })
-            wx.setStorage({
-              key: 'userId',
-              data: res.data.userId
-            })
-            wx.redirectTo({
-              url: '../main/main',
-            })
-          }
-        })
-      }
+      this.login(res);
     } else {
-      //用户按了拒绝按钮
       wx.showModal({
         title: '警告',
         content: '您点击了拒绝授权，将无法进入小程序，请授权之后再进入!!!',
@@ -103,6 +31,45 @@ Page({
           if (res.confirm) {
             console.log('用户点击了“返回授权”')
           }
+        }
+      })
+    }
+  },
+  login: function (res) {
+    app.globalData.userInfo = res.userInfo;
+    var param = {
+      code: app.globalData.code,
+      userInfo: {
+        userInfo: {
+          country: res.userInfo.country,
+          gender: res.userInfo.gender,
+          province: res.userInfo.province,
+          city: res.userInfo.city,
+          avatarUrl: res.userInfo.avatarUrl,
+          nickName: res.userInfo.nickName,
+          language: res.userInfo.language
+        },
+        signature: res.signature,
+        errMsg: res.errMsg,
+        encryptedData: res.encryptedData,
+        iv: res.iv,
+        rawData: JSON.stringify(JSON.parse(res.rawData))
+      }
+    }
+    if (res.userInfo) {
+      login.wxBindLogin(param, function (res) {
+        if (res.errno == 0) {
+          wx.setStorage({
+            key: "token",
+            data: res.data.token
+          })
+          wx.setStorage({
+            key: 'userId',
+            data: res.data.userId
+          })
+          wx.redirectTo({
+            url: '../main/main',
+          })
         }
       })
     }
